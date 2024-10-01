f9617066c99068501c6c94f4e167bf4e23b3c8ae
AFz <0xafz@proton.me>
2024-10-02 02:05:40 +0330

    Add nova to repo!

---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+import yaml
+import sys
+
+def print_help():
+    print("Usage: terraform show --json | python3 main.py")
+
+def parse_input():
+    if not sys.stdin.isatty():
+        input_data = sys.stdin.read().strip()
+        return json.loads(input_data)
+    else:
+        print_help()
+        sys.exit(1)
+
+def create_inventory(terraform_data):
+    inventory = {
+        'all': {
+            'hosts': {}
+        }
+    }
+    
+    for resource in terraform_data['values']['root_module']['resources']:
+        if resource['type'] == 'openstack_compute_instance_v2':
+            ip = resource['values']['access_ip_v4']
+            inventory['all']['hosts'][ip] = {
+                'ansible_user': 'root',
+                'ansible_port': 22
+            }
+    return inventory
+
+def write_inventory_to_file(inventory):
+    with open('inventory.yml', 'w') as file:
+        yaml.dump(inventory, file, indent=2)
+
+def main():
+    terraform_data = parse_input()
+    inventory = create_inventory(terraform_data)
+    write_inventory_to_file(inventory)
+
+if __name__ == "__main__":
+    main()
+
