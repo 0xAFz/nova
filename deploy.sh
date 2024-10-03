1866997202836b325fce7064814bb997f68c02bd
AFz <afz.badie@gmail.com>
2024-10-04 00:04:25 +0330

    Improve env vars handling

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./.env
+
 if [ ! -d ".venv" ]; then
 	python3 -m venv .venv
 	source .venv/bin/activate
@@ -7,21 +9,6 @@ if [ ! -d ".venv" ]; then
 else
 	source .venv/bin/activate
 fi
-
-export XUI_USERNAME=admin
-export XUI_PASSWORD=admin
-export XUI_PORT=2053
-
-export DOMAIN=domain.tld
-export CLOUDFLARE_EMAIL=mail@domain.tld
-export CLOUDFLARE_API_KEY=xyz
-export ZONE_ID=xyz
-
-export TF_VAR_OS_USERNAME=openstack_username
-export TF_VAR_OS_TENANT_NAME=openstack_username
-export TF_VAR_OS_PASSWORD=openstack_password
-export TF_VAR_OS_AUTH_URL=openstack_auth_url
-export TF_VAR_OS_REGION_NAME=openstack_region
 
 check_ping() {
 	ansible all -i inventory.yml -m ping --timeout 1
@@ -32,11 +19,12 @@ terraform init && \
 	terraform apply -auto-approve && \
 	terraform show -json | python3 main.py
 
+if [ "$?" -eq 0 ]; then
+    until check_ping; do
+        echo "Waiting for VM to become reachable..."
+        sleep 1
+    done
 
-until check_ping; do
-	echo "Waiting for VM to become reachable..."
-	sleep 1
-done
-
-ansible-playbook -i inventory.yml xui.yml
+    ansible-playbook -i inventory.yml xui.yml
+fi
 
