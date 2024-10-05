3b339dc57db5c5476e686d355ed7c0ec73254eee
AFz <afz.badie@gmail.com>
2024-10-06 02:35:21 +0330

    refactor: use best practices for variables handling

--- main.tf
+++ main.tf
@@ -1,56 +1,18 @@
-terraform {
-required_version = ">= 0.14.0"
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.53.0"
-    }
-  }
-}
-
-provider "openstack" {
-  user_name   = var.OS_USERNAME
-  tenant_name = var.OS_TENANT_NAME
-  password    = var.OS_PASSWORD
-  auth_url    = var.OS_AUTH_URL
-  region      = var.OS_REGION_NAME
-}
-
-variable "OS_USERNAME" {
-  type        = string
-}
-
-variable "OS_TENANT_NAME" {
-  type        = string
-}
-
-variable "OS_PASSWORD" {
-  type        = string
-}
-
-variable "OS_AUTH_URL" {
-  type        = string
-}
-
-variable "OS_REGION_NAME" {
-  type        = string
-}
-
 resource "openstack_compute_keypair_v2" "nova_pk" {
   name       = "nova_pk"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = file(var.public_key_path)
 }
 
 resource "openstack_compute_instance_v2" "nova" {
-  name            = "nova"
-  image_name      = "Ubuntu-24.04-amd64"
-  flavor_name     = "flavor_name"
-  key_pair        = openstack_compute_keypair_v2.nova_pk.name
+  name        = var.machine_name
+  image_name  = var.image_name
+  flavor_name = var.flavor_name
+  key_pair    = openstack_compute_keypair_v2.nova_pk.name
 
   security_groups = ["allow_all"]
 
   network {
-    name = "Internet-18"
+    name = var.network_name
   }
 
   user_data = <<-EOF
