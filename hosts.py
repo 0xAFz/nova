7b8835e8743c7326dc3380ac7b9e6825cc9784d3
AFz <0xafz@proton.me>
2024-10-06 19:06:44 +0330

    refactor: use outputs for generating inventory

--- hosts.py
+++ hosts.py
@@ -2,8 +2,10 @@ import json
 import yaml
 import sys
 
+
 def print_help():
     print("Usage: terraform show -json | python3 hosts.py")
+
 
 def parse_input():
     if not sys.stdin.isatty():
@@ -13,31 +15,30 @@ def parse_input():
         print_help()
         sys.exit(1)
 
-def create_inventory(terraform_data):
-    inventory = {
-            'all': {
-                'hosts': {}
-                }
-            }
 
-    for resource in terraform_data['values']['root_module']['resources']:
-        if resource['type'] == 'openstack_compute_instance_v2':
-            ip = resource['values']['access_ip_v4']
-            inventory['all']['hosts'][ip] = {
-                    'ansible_user': 'root',
-                    'ansible_port': 22
-                    }
+def create_inventory(terraform_data):
+    inventory = {"all": {"hosts": {}}}
+
+    ip = terraform_data["instance_public_ip"]["value"]
+
+    inventory["all"]["hosts"][ip] = {
+        "ansible_user": "root",
+        "ansible_port": 22,
+    }
+
     return inventory
 
+
 def write_inventory_to_file(inventory):
-    with open('inventory.yml', 'w') as file:
+    with open("inventory.yml", "w") as file:
         yaml.dump(inventory, file, indent=2)
+
 
 def main():
     terraform_data = parse_input()
     inventory = create_inventory(terraform_data)
     write_inventory_to_file(inventory)
 
+
 if __name__ == "__main__":
     main()
-
