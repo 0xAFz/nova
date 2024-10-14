9a335709fffc2e10907e9fc7f3ec3eb145ffcfe4
AFz <0xafz@proton.me>
2024-10-14 17:44:50 +0330

    feat: get subdomain from env vars and get certs with letsencrypt

--- inbound.sh
+++ inbound.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SUBDOMAIN="nova.$DOMAIN"
+FULLDOMAIN="$SUBDOMAIN.$DOMAIN"
 db_path="/etc/x-ui/x-ui.db"
 
 inbound_id=1
@@ -15,7 +15,7 @@ listen=""
 port=2087
 protocol="vless"
 settings="{ \"clients\": [ { \"id\": \"1dc7571c-ad68-4afb-a7d3-9d4573c23967\", \"flow\": \"\", \"email\": \"nova\", \"limitIp\": 0, \"totalGB\": 0, \"expiryTime\": 0, \"enable\": true, \"tgId\": \"\", \"subId\": \"p4dp8saxu8gabbkw\", \"reset\": 0 } ], \"decryption\": \"none\", \"fallbacks\": [] }"
-stream_settings="{ \"network\": \"httpupgrade\", \"security\": \"tls\", \"externalProxy\": [ { \"forceTls\": \"same\", \"dest\": \"wWw.sPeEdTesT.net\", \"port\": \"$port\", \"remark\": \"\" } ], \"tlsSettings\": { \"serverName\": \"$SUBDOMAIN\", \"minVersion\": \"1.0\", \"maxVersion\": \"1.3\", \"cipherSuites\": \"\", \"rejectUnknownSni\": false, \"disableSystemRoot\": false, \"enableSessionResumption\": false, \"certificates\": [ { \"certificateFile\": \"/etc/nginx/ssl/nova/pubkey.pem\", \"keyFile\": \"/etc/nginx/ssl/nova/private.key\", \"ocspStapling\": 3600, \"oneTimeLoading\": false, \"usage\": \"encipherment\", \"buildChain\": false } ], \"alpn\": [ \"h2\", \"http/1.1\" ], \"settings\": { \"allowInsecure\": false, \"fingerprint\": \"firefox\" } }, \"httpupgradeSettings\": { \"acceptProxyProtocol\": false, \"path\": \"/api/v2/shop/products?ed=2048\", \"host\": \"$SUBDOMAIN\", \"headers\": {} } }"
+stream_settings="{ \"network\": \"httpupgrade\", \"security\": \"tls\", \"externalProxy\": [ { \"forceTls\": \"same\", \"dest\": \"wWw.sPeEdTesT.net\", \"port\": \"$port\", \"remark\": \"\" } ], \"tlsSettings\": { \"serverName\": \"$FULLDOMAIN\", \"minVersion\": \"1.0\", \"maxVersion\": \"1.3\", \"cipherSuites\": \"\", \"rejectUnknownSni\": false, \"disableSystemRoot\": false, \"enableSessionResumption\": false, \"certificates\": [ { \"certificateFile\": \"/etc/letsencrypt/live/$FULLDOMAIN/fullchain.pem\", \"keyFile\": \"/etc/letsencrypt/live/$FULLDOMAIN/privkey.pem\", \"ocspStapling\": 3600, \"oneTimeLoading\": false, \"usage\": \"encipherment\", \"buildChain\": false } ], \"alpn\": [ \"h2\", \"http/1.1\" ], \"settings\": { \"allowInsecure\": false, \"fingerprint\": \"firefox\" } }, \"httpupgradeSettings\": { \"acceptProxyProtocol\": false, \"path\": \"/api/v2/shop/products?ed=2048\", \"host\": \"$FULLDOMAIN\", \"headers\": {} } }"
 tag="inbound-2087"
 sniffing="{ \"enabled\": true, \"destOverride\": [ \"http\", \"tls\", \"quic\", \"fakedns\" ], \"metadataOnly\": false, \"routeOnly\": false }"
 
